a2e3c3bd5e09dd5e9bb4d27016ef224bd55d43f4
armmani <arm.maniii@gmail.com>
2025-06-18 15:25:41 +0700

    ย้ายตัว validate(yup) ไปไฟล์ใหม่ validations > validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,8 @@
 import express from "express";
 // Controllers
 import { login, register } from "../controllers/auth.js";
-// Validate with yup
-import { object, ref, string } from "yup";
+import { validate } from "../validations/validator.js";
 
-const registerSchema = object({
-  email: string().email("Incorrect Email").required("Enter your Email"),
-  name: string().min(2, "Name must be at least 2 letters"),
-  password: string().min(6, "Password must be at least 6 letters"),
-  confirmPassword: string().oneOf(
-    [ref("password"), null],
-    "Password is not Matched"
-  ),
-});
-
-const validate = (schema) => async (req, res, next) => {
-  // code body
-  try {
-    await schema.validate(req.body, { abortEarly: false });
-    next();
-  } catch (error) {
-    console.log(error)
-    next(error);
-  }
-};
 
 const router = express.Router();
 
---
+++ validations/validator.js
@@ -0,0 +1,26 @@
+// Validate with yup
+import { object, ref, string } from "yup";
+
+export const registerSchema = object({
+  email: string().email("Incorrect Email").required("Enter your Email"),
+  name: string().min(2, "Name must be at least 2 letters"),
+  password: string().min(6, "Password must be at least 6 letters"),
+  confirmPassword: string().oneOf(
+    [ref("password"), null],
+    "Password is not Matched"
+  ),
+});
+
+export const validate = (schema) => async (req, res, next) => {
+  // code body
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    const errMsg = error.errors.map((item) => item);
+    const errTxt = errMsg.join(",");
+    console.log(errTxt);
+    const mergeErr = new Error(errTxt);
+    next(mergeErr);
+  }
+};
