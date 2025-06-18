fe9b1dd15313873dd918318038c4878a1fe02861
armmani <arm.maniii@gmail.com>
2025-06-18 14:36:09 +0700

    step 1, 2 done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,37 @@
-export const register = (req, res) => {
-  // TODO Overview Register
-  /*
+import prisma from "../config/prisma.js";
+import { createError } from "../utils/createError.js";
+
+export const register = async (req, res, next) => {
+  try {
+    // TODO Overview Register
+    /*
     1.Check body
     2.Check Email in DB
-    3.Ecrypt Password => bcryptjs
+    3.Encrypt Password => bcryptjs
     4.Save to DB -> Prisma
     5.Response
   */
 
-  // Step 1 Check Body
-  console.log(req.body);
+    // Step 1 Check Body
+    console.log(req.body);
+    const { email, name, password } = req.body;
 
-  res.json({ message: "This is Register" });
+    // Step 2 Check Email in DB
+    const user = await prisma.user.findFirst({
+      where:{
+        email:email
+      }
+    })
+    console.log(user)
+    if(user) {
+      createError(400, "Email already Exist")
+    }
+    // Step 3 Encrypt Password
+
+    res.json({ message: "This is Register" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const login = (req, res) => {
