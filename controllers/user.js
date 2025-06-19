2c187d072ef8ea447013fbececb877c6b095c918
armmani <arm.maniii@gmail.com>
2025-06-19 10:49:05 +0700

    แก้ GetMe done

--- controllers/user.js
+++ controllers/user.js
@@ -54,6 +54,24 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
+export const getMe = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    console.log(id);
+    const user = await prisma.user.findFirst({
+      where: {
+        id: Number(id),
+      },
+      omit: {
+        password: true,
+      },
+    });
+
+    res.json({ result: user });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const readUser = (req, res) => {
   res.json({ message: "This is Read User" });
